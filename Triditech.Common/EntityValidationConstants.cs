7911d9909a6de7abb38926a017ad14c53ab3e3bc
boyanroyachki <106555994+boyanroyachki@users.noreply.github.com>
2024-02-25 13:54:22 +0200

    Added a static class that will hold the entity validation constants. Placed it in a common class library.

---
+++ Triditech.Common/EntityValidationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Triditech.Common
+{
+    /// <summary>
+    /// Static class that holds more static classes, named after the application entities. 
+	/// The properties are constant values that are used to validate the entity properties.
+    /// </summary>
+    public static class EntityValidationConstants
+    {
+    }
+}
